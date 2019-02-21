34a7f320659d72e4ec5558dd3e822b7c87ff9e2a
Deven Douglas <devendouglas@gmail.com>
2019-02-20 18:46:42 -0800

    figured out class hierarchy

--- ZombieDash/Actor.h
+++ ZombieDash/Actor.h
@@ -4,5 +4,32 @@
 #include "GraphObject.h"
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class Actor: public GraphObject
+{
+public:
+	Actor();
+	doSomething();
+private:
+}
 
+class Biological : public Actor
+{
+}
+//class Zombie: public Biological
+class Penelope: public Biological
+//class SmartZombie:public Zombie
+//class DumbZombie:public Zombie
+//class Citizen: public Biological
+class Item: public Actor
+//class Goodie: public Item
+//class GasCan: public Goodie
+//class Vaccine: public Goodie
+//class LandmineGoodie: public Goodie
+class Hazard: public Item
+//class Pit: public Hazard
+//class Flame: public Hazard
+//class Vomit: public Hazard
+//class Landmine: public Hazard
+class Wall: public Hazard
+//class Exit: public Hazard
 #endif // ACTOR_H_
